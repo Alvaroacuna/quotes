fce6ab183bafdbc4490c30e76322b07e1e835793
Alvaro Acuña <sebastian20.a.m@gmail.com>
2021-02-18 13:40:34 -0300

    Se añadió try y catch en el formulario

--- routes/quote.js
+++ routes/quote.js
@@ -2,43 +2,28 @@ const { Router } = require('express');
 const { QuoteNew } = require('../db');
 
 
+
+
 const router = Router();
 
-/*const quotes = [
-    {
-        author: "Diego Maradona",
-        text: "Ehhhh........."
-    },
-    {
-        author: "Cristiano Ronaldo",
-        text: "Siiiuuuuuuuuuu"
-    },
-    {
-        author: "Lionel Messi",
-        text: "Lo siento, pero no se me da"
-    },
-    {
-        author: "Alexis Sánchez",
-        text: "Mi intindi"
-    }
-
-]; */
 
 router.get("/", async (req, res) => {
     const quotesnew = await QuoteNew.findAll();
 
+    let errores = req.flash("error");
     let mensaje = req.flash('mensaje');
-        console.log(mensaje);
+    console.log(mensaje);
 
-    res.render("principal", {quotes: quotesnew, mensaje : mensaje})
+    res.render("principal", { quotes: quotesnew, mensaje: mensaje, errores: errores })
 });
 
 
-router.get('/quotes', function(req, res) {
-    res.render('quotes', {quotes:quotes});
+router.get('/quotes', async (req, res) => {
+    const quotesnew = await QuoteNew.findAll();
+    res.render('quotes', { quotes : quotesnew});
 });
 
-router.get('/borrar/:id', async (req,res) => {
+router.get('/borrar/:id', async (req, res) => {
 
     const quote = await QuoteNew.findByPk(req.params.id);
     console.log(quote);
@@ -47,10 +32,10 @@ router.get('/borrar/:id', async (req,res) => {
 
     req.flash('mensaje', `El quote ${quote.text} fue eliminado de la base de datos.`);
 
-    res.json({Estado: true, Mensaje: `El quote ${quote.id} fue eliminado de la base de datos.` });
+    res.json({ Estado: true, Mensaje: `El quote ${quote.id} fue eliminado de la base de datos.` });
 });
 
-router.post('/actualizar/:id', async (req,res) => {
+router.post('/actualizar/:id', async (req, res) => {
 
     console.log("Llegó el ID" + req.params.id);
     console.log("Del formulario Editado llegó este author: " + req.body.author);
@@ -69,35 +54,46 @@ router.post('/actualizar/:id', async (req,res) => {
 });
 
 
-router.post('/quotes', async (req,res) => {
-    const new_quotes = await QuoteNew.create({
-        author: req.body.author,
-        text: req.body.text
-    });
-    
-    req.flash('mensaje', `El quote ${new_quotes.text} fue creado en la base de datos.`);
+router.post('/quotes', async (req, res) => {
 
-
-    const quotesnew = await QuoteNew.findAll();
+    try {
+        if (req.body.author == '')
+            throw new Error('El autor no puede ser vacío, debes de escribir uno');
+        if (req.body.text == '')
+            throw new Error('El texto no puede ser vacío, debes de escribir uno');
+        const new_quotes = await QuoteNew.create({
+            author: req.body.author,
+            text: req.body.text
+        });
+        req.flash('mensaje', `El quote ${new_quotes.text} fue creado en la base de datos.`);
+    } catch (err) {
+        console.log(err.message);
+        req.flash('error', err.message);
+    }
 
     /*let info = req.body;
     quotes.push({author: info.author, text: info.text})
     console.log(quotes); */
-    res.render('quotes', {quotes : quotesnew});
+    res.redirect('/');
 
 });
+
+
+
+
+
 
 router.get("/editar/:id", async (req, res) => {
 
     console.log("Llegó el ID" + req.params.id);
     const quote = await QuoteNew.findByPk(req.params.id);
     let mensaje = req.flash('mensaje');
-        console.log(mensaje);
+    console.log(mensaje);
 
-    
-    res.render("editar", {quote: quote, mensaje : mensaje})
+
+    res.render("editar", { quote: quote, mensaje: mensaje })
 });
 
-  
+
 
 module.exports = router;
